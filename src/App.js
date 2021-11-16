e6f8cf5e0c0276333d0188fd934d7c267a8e5a56
Syed Shahid <syedshahid1215@gmail.com>
2021-11-16 18:20:09 +0500

    completed TopBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import TopBar from "./components/TopBar/TopBar";
+import "./App.css"
 
 function App() {
   return (
     <div>
-      Hello
+      <TopBar />
     </div>
   );
 }
---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
+import "./TopBar.css";
+
+function TopBar() {
+    return (
+        <div className="topBar">
+            <div className="topBarWrapper">
+                <div className="topLeft">
+                    <span className="logo">My Admin Dashboard</span>
+                </div>
+                <div className="topRight">
+                    <div className="topBarIconContainer">
+                        <NotificationsNoneIcon />
+                        <span className="iconCount">2</span>
+                    </div>
+                    <div className="topBarIconContainer">
+                        <LanguageIcon />
+                        <span className="iconCount">4</span>
+                    </div><div className="topBarIconContainer">
+                        <SettingsIcon />
+                    </div>
+                    <img className="topAvatar" src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="sample" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TopBar;
