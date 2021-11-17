46e29c3f300da7593075a0f24e8164ba33784e40
Syed Shahid <syedshahid1215@gmail.com>
2021-11-17 15:47:57 +0500

    make charts dynamic

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -10,70 +10,19 @@ import {
     ResponsiveContainer 
 } from 'recharts';
 
-const data = [
-    {
-        name: 'Jan',
-        "Active Users": 4000,
-    },
-    {
-        name: 'Feb',
-        "Active Users": 3000,
-    },
-    {
-        name: 'Mar',
-        "Active Users": 5000,
-    },
-    {
-        name: 'Apr',
-        "Active Users": 4000,
-    },
-    {
-        name: 'May',
-        "Active Users": 3000,
-    },
-    {
-        name: 'Jun',
-        "Active Users": 2000,
-    },
-    {
-        name: 'Jul',
-        "Active Users": 4000,
-    },
-    {
-        name: 'Aug',
-        "Active Users": 3000,
-    },
-    {
-        name: 'Sep',
-        "Active Users": 4000,
-    },
-    {
-        name: 'Oct',
-        "Active Users": 1000,
-    },
-    {
-        name: 'Nov',
-        "Active Users": 4000,
-    },
-    {
-        name: 'Dec',
-        "Active Users": 3000,
-    },
-];
 
-
-function Chart() {
+function Chart({title, data, dataKey, grid}) {
     return (
         <div className="chart">
-            <h3 className="chartTitle">User Analytics</h3>
+            <h3 className="chartTitle">{title}</h3>
             {/* if the width is 4 units, then the height will be 1 unit */}
             {/* if the width is 400px, then the height will be 100px */}
             <ResponsiveContainer width={"100%"} aspect={4 /1}>
                 <LineChart data={data}>
                     <XAxis dataKey={"name"} stroke="#5550bd" />
-                    <Line type="monotone" dataKey="Active Users" stroke="#5550bd" />
+                    <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
                     <Tooltip />
-                    <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
+                    {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
                     {/* <Legend /> */}
                 </LineChart>
             </ResponsiveContainer>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import "./Home.css";
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo';
 import Chart from '../../components/Chart/Chart';
-
+import {usersData} from "../../components/Chart/dummyData";
 function Home() {
     return (
         <div className="home">
             <FeaturedInfo />
-            <Chart />
+            <Chart 
+                data={usersData} 
+                title="User Analytics" 
+                dataKey="Active Users"
+                grid
+            />
         </div>
     )
 }
