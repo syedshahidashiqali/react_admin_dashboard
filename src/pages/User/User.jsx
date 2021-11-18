e5c35022385d346a9c99afee46630ad9e500f6ed
Syed Shahid <syedshahid1215@gmail.com>
2021-11-18 15:51:33 +0500

    completed user update route and right pane of user

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -4,6 +4,7 @@ import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import PublishIcon from '@mui/icons-material/Publish';
 
 function User() {
     return (
@@ -46,10 +47,46 @@ function User() {
                         </div>
                     </div>
                 </div>
-                <div className="userUpdate"></div>
+                <div className="userUpdate">
+                    <span className="userUpdateTitle">Edit</span>
+                    <form action="" className="userUpdateForm">
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Username</label>
+                                <input className="userUpdateInput" type="text" placeholder="annabeck99" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Full Name</label>
+                                <input className="userUpdateInput" type="text" placeholder="Anna Becker" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Email</label>
+                                <input className="userUpdateInput" type="text" placeholder="annabeck99@gmail.com" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Phone</label>
+                                <input className="userUpdateInput" type="text" placeholder="+1 123 456 67" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Address</label>
+                                <input className="userUpdateInput" type="text" placeholder="New York | USA" />
+                            </div>
+                        </div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img className="userUpdateImg" src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                                <label htmlFor="file">
+                                    <PublishIcon className="userUpdateIcon" />
+                                </label>
+                                <input type="file" name="" id="file" />
+                            </div>
+                            <button className="userUpdateButton">Update</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
 }
 
-export default User
+export default User;
